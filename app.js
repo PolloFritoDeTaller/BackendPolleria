735bfa90191e1f29aa83648d57206bd6c4b6ecee
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-12-15 19:42:06 -0400

    Arreglando de nuevo

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(morgan('dev'));
 connectToMongoDB();
 
 app.use(cookieParser());
-app.use('/uploads', express.static('https://backendpolleria.onrender.com/uploads'));
+app.use('/uploads', express.static('https://backendpolleria-aumu.onrender.com/uploads'));
 
 const PORT = process.env.PORT || 3000;
 
